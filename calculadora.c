4b7fbf4139a45d596fbc2afbd165ac0cbe628b0d
PedroLuzon <pedroluzon2a@gmail.com>
2017-04-28 19:23:40 +0200

    suma y resta

--- calculadora.c
+++ calculadora.c
@@ -10,10 +10,23 @@ double iva(float a);
 
 int main ( int argc, char *argv[]){
      printf("----------------------------MENU--------------------------------"\n);
-     printf("1. Suma"\n);       
-     printf("2. Resta"\n);  
-     printf("3. Multiplicacion"\n);  
-     printf("4. Division"\n);  
+     printf("1. Suma"\n);
+     printf("2. Resta"\n);
+     printf("3. Multiplicacion"\n);
+     printf("4. Division"\n);
      printf("5. Potencia\n");
      printf("6. Porcentaje\n");
 }
+
+double multiplicacion(float a, float b){
+  return a * b;
+}
+
+float suma(float a, float b){
+	return a+b;
+}
+
+float resta ( float a, float b) { 
+        return a-b;
+}
+
